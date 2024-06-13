8f4cec024f033d7f740986ae72f6dbea25a8406e
zaizen24 <achmadzainunq@gmail.com>
2024-06-13 21:09:57 +0700

    zai 2

--- lib/services/login_service.dart
+++ lib/services/login_service.dart
@@ -1,14 +1,32 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class LoginService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   Future<void> login(String email, String password) async {
     try {
-      await _auth.signInWithEmailAndPassword(
+      // Attempt to sign in with email and password
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
         email: email,
         password: password,
       );
+
+      // Get the user ID
+      String userId = userCredential.user!.uid;
+
+      // Check if the user exists in the "users" collection
+      DocumentSnapshot userDoc =
+          await _firestore.collection('users').doc(userId).get();
+
+      if (!userDoc.exists) {
+        // User is not in the "users" collection, sign out and throw an exception
+        await _auth.signOut();
+        throw Exception('User does not have access. Please contact support.');
+      }
+
+      // User is in the "users" collection, proceed with login
     } on FirebaseAuthException catch (e) {
       String message;
       if (e.code == 'user-not-found') {
